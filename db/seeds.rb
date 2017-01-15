2bf1429b6a10489605c2d9052ce9db94d160ed64
Haico27 <haico.wensink@gmail.com>
2017-01-15 13:51:33 +0100

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.delete_all
+Artist.delete_all
+
+song1 = Song.create( { name: "In So Many Ways", album: "No Substance", releasedate: 1998 } )
+song2 = Song.create( { name: "My Life Inside Your Heart", album: "The Unraveling", releasedate: 2001 } )
+song3 = Song.create( { name: "Nailed Shut", album: "Hard Rock Bottom", releasedate: 2002 } )
+song4 = Song.create( { name: "The Kids Aren't Alright", album: "Americana", releasedate: 1998 } )
+song5 = Song.create( { name: "No Control", album: "No Control", releasedate: 1989 } )
+
+artist1 = Artist.create(name: "Bad Religion")
+artist2 = Artist.create(name: "Rise Against")
+artist3 = Artist.create(name: "No Use For A Name")
+artist4 = Artist.create(name: "The Offspring")
+
+artist1.songs << [song1]
+artist1.save
+
+artist2.songs << [song2]
+artist2.save
+
+artist3.songs << [song3]
+artist3.save
+
+artist4.songs << [song4]
+artist4.save
+
+artist1.songs << [song5]
+artist1.save
